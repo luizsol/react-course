b3fb0df1b539a69ab5718f1c80b756b52efef7dc
Luiz Eduardo Sol <luizedusol@gmail.com>
2018-05-03 17:27:53 -0300

    Finishes the module 19 (class 336)

--- burguer-creator/src/containers/Auth/Auth.js
+++ burguer-creator/src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import Button from '../../components/UI/Buttton/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
+import {checkValidity} from '../../shared/utility';
 
 class Auth extends Component {
   state = {
@@ -50,40 +51,13 @@ class Auth extends Component {
     }
   }
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.minLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      //
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    return isValid;
-  }
-
   inputChangedHandler = (event, controlName) => {
     const updatedControls = {
       ...this.state.controls,
       [controlName]: {
         ...this.state.controls[controlName],
         value: event.target.value,
-        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
         touched: true,
       }
     }
